7aab249fec9c3d7039fdf7164ea8a1930f8fe400
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-10-19 15:30:24 -0400

    string practice

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php 
 
-echo "hello, world";
+$emailLabel = 'email: ';
+$emailData = 'test@gmail.com';
+
+// echo $emailLabel.$emailData;
+
+// echo "Hey my email is $emailData"
+
+// echo "the ninja screamed \"whaaa\"";
+
+// echo $emailData[0]
+echo strtoupper($emailLabel);
+echo strlen($emailData);
+
+echo str_replace('m', 'w', $emailData)
+
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Project</title>
+</head>
+<body>
+    
+</body>
+</html>
